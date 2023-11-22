8164311814b7112f529f5da061e0d1ec3b020493
Daan Michielsen <90918416+DaanMichielsen@users.noreply.github.com>
2023-11-22 18:21:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import torch
 import os
 import glob
+from IPython.display import display
 # Step 1: Install Requirements
 st.title("Detecting & Tracking Vespa Velutina (Yellow Legged Hornet) Streamlit App")
 
