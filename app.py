625f94a66ef91fd6cb76cba62e64e5e58d71190e
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-11-22 18:16:36 +0100

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import os
-from PIL import Image
-from roboflow import Roboflow
-import torch
-import glob
-from IPython.display import display
+
 
 # Step 1: Install Requirements
 st.title("Detecting & Tracking Vespa Velutina (Yellow Legged Hornet) Streamlit App")
