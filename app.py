3c9310876cb7b488a8b1b61535b827ca6ea7b3f2
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-11-27 13:46:05 +0100

    streamlit adding the video

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-
-# Step 1: Install Requirements
+from PIL import Image
 st.title("Detecting & Tracking Vespa Velutina (Yellow Legged Hornet) Streamlit App")
+img = Image.open('C:/AI/AI Project/AIProject-Team7/Images/Bees/Checked and uploaded 13Nov23/abeille_1.jpg')
+
+st.image(img)
+video_file = open('C:/AI/AI Project/AIProject-Team7/BeesAndHornets.mp4','rb')
+
+#video_bytes = video_file.read()
+
+st.video(video_file)
 
